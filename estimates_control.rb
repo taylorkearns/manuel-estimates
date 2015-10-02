f8dffc4969e4fc32e8b5432d7610a889bd08c7db
Taylor Kearns <tkearns@gmail.com>
2015-10-02 14:19:04 -0400

    Code cleanup, log errors, remove status update

--- estimates_control.rb
+++ estimates_control.rb
@@ -1,13 +1,14 @@
 require "rubygems"
 require "daemons"
 require "twitter"
+require "logger"
 require_relative "environment.rb"
 
 Daemons.run_proc("estimates.rb") do
   class Estimates
     attr_accessor :twitter, :search_term, :anti_search_term
 
-    def initialize(search_term="Estimates", anti_search_term="Free Estimates")
+    def initialize(options)
       @twitter = Twitter::REST::Client.new do |config|
         config.consumer_key = EnvironmentVars::TWITTER_CONSUMER_KEY
         config.consumer_secret = EnvironmentVars::TWITTER_CONSUMER_SECRET
@@ -15,45 +16,31 @@ Daemons.run_proc("estimates.rb") do
         config.access_token_secret = EnvironmentVars::TWITTER_ACCESS_TOKEN_SECRET
       end
 
-      @search_term = search_term
-      @anti_search_term = anti_search_term
+      @search_term = options[:search_term]
+      @anti_search_term = options[:anti_search_term]
 
       while true
         search_and_retweet
-        sleep 43_200
+        sleep 43_200 # twice per day
       end
     end
 
     def search_and_retweet
-      tweets = twitter.search search_term,
-        lang: "en",
-        result_type: "recent"
+      tweets = twitter.search search_term, lang: "en", result_type: "recent"
+      qualifying_tweets = tweets.to_a.keep_if { |status| status_qualifies(status) }
+      tweet = qualifying_tweets.first
+      twitter.retweet tweet.id
 
-      status = tweets.to_a.keep_if { |status| status_qualifies(status) }.first
-      #tweet = build_tweet(status)
-      #twitter.update(tweet) unless tweet.nil?
-      twitter.retweet status.id
-
-    rescue Twitter::Error::Unauthorized => error
-      puts "#{error.class}: #{error.message}"
+    rescue Twitter::Error => error
+      logger = Logger.new(STDERR)
+      logger.error "#{error.class}: #{error.message}"
     end
 
     def status_qualifies(status)
       status.text.match(/#{search_term}/) &&
       !(status.text.match(/#{anti_search_term}/))
     end
-
-    def build_tweet(status)
-      words = status.text.squeeze(" ").split(" ")
-      i = words.index search_term
-      p i
-      if i > 1
-        "#{words[i - 2]} #{words[i - 1]} me!"
-      elsif i == 1
-        "#{words.first} me!"
-      end
-    end
   end
 
-  Estimates.new()
+  Estimates.new(search_term: "Estimates", anti_search_term: "Free Estimates")
 end
